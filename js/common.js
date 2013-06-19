ef7bf8223e785075429da8d82804b7e2bf026ddd
Artur Moroz <morozarturv@gmail.com>
2013-06-19 14:01:18 +0300

    some fixes

--- js/common.js
+++ js/common.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
 
+//colorbox
 jQuery.extend(jQuery.colorbox.settings, {
 	current: "{current}/{total}"
 });
-
 $('.js-cb').colorbox({
 	rel:'gallery', 
 	slideshow:true, 
 	slideshowAuto: false, 
 	opacity: 0.8
 });
-
 $(document).bind('cbox_complete', function(){
     var head = $.colorbox.element().attr('data-head');
     var link = $.colorbox.element().attr('data-link');
@@ -18,6 +17,22 @@ $(document).bind('cbox_complete', function(){
     $('#cboxLoadedContent').append('<div id="cboxLoad"><a href=' + link + '>Скачать в HQ</a></div>');
 });
 
+//calendar select
+$('.js-datepicker').datepicker({
+  inline: true,
+  monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+  'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+  monthNamesShort: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+  'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+  dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+  dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+  dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+  changeYear: true,
+  minDate: "-3Y", 
+  maxDate: "0"
+});
+
+//scroll
 $(window).scroll(function() {
   if ($('body').scrollTop() > 180) {
   	$('.wrap').addClass('wrap_scroll');
